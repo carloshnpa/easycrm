3c22e0c6de4763e4f1edbdf9121f6c2224f5f963
Carlos.Abreu <carlos.abreu@athonenergia.com.br>
2024-05-29 14:36:42 -0300

    add clarity monitor

--- app/(protected)/app/layout.tsx
+++ app/(protected)/app/layout.tsx
@@ -2,6 +2,7 @@ import SideBar from "@/components/app/layouts/SideBar";
 import NavBar from "@/components/app/layouts/NavBar";
 import {Separator} from "@/components/ui/separator";
 import {Toaster} from "@/components/ui/toaster";
+import Script from "next/script";
 
 export default function RootLayout({
                                        children,
@@ -21,6 +22,15 @@ export default function RootLayout({
                 </div>
             </div>
             <Toaster/>
+            <Script id="clarity-script" strategy="afterInteractive">
+                {process.env.ENVIRONMENT === 'production' && `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+            </Script>
         </>
     );
 }
